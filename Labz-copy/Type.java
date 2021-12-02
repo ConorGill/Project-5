d17edbdebd3c33ab4ab5026483ad22db23d9ef6a
ConorGill <94633176+ConorGill@users.noreply.github.com>
2021-12-02 03:11:44 -0500

    Create Type.java
    Again made values to represent the different types of cards.

---
+++ Labz-copy/Type.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Colors - write a description of the enum class here
+ * Made a value for every possible type of color
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Type
+{
+    CLUBS,SPADES,DIAMONDS,HEARTS
+}
