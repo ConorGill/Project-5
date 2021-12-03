ccadb85c8f0ee322a9c5fd50d892f0b7c9fd88c6
ConorGill <94633176+ConorGill@users.noreply.github.com>
2021-12-02 23:52:03 -0500

    Update Player.java
    Added quite a bit now there is a list to represent the hand of the player.  There is a method to add to this hand as well as one to print it out.

--- Labz-copy/Player.java
+++ Labz-copy/Player.java
@@ -1,26 +1,53 @@
-
 /**
  * Write a description of class Player here.
  * An object meant to simulate a Player with their name and current balance
  */
+import java.util.ArrayList;
 public class Player
 {
     private String name;
     private int balance;
-    
+    private ArrayList<Card> hand;
+    /*Desc: Deafult Constructor for the player
+     */
     public Player()
     {
-        name="John Doe";
-        balance=5000;
+        this.name="John Doe";
+        this.balance=5000;
+        this.hand=new ArrayList<Card>();
     }
+    /*Desc: Constructor that inputs the values for the name of the player and their balance.
+     * @param Name: name of the player
+     * @param Balance: Balance of the player
+     */
     public Player(String Name,int Balance){
-        name=Name;
-        balance=Balance;
+        this.name=Name;
+        this.balance=Balance;
+        this.hand=new ArrayList<Card>();
     }
+    /*
+     * Desc:Gets the balance of the player
+     */
     public int getBalance(){
         return this.balance;
     }
+    /*
+     * Desc:Gets the name of the player.
+     */
     public String getName(){
         return this.name;
+    }
+    /*
+     * Desc:Adds card to hand
+     * @param C: The Card being added
+     */
+    public void addCard(Card C){
+        this.hand.add(C);
+    }
+    //Desc: Method that prints out the hand of the player. 
+    public void printHand(){
+       for(Card c:this.hand){
+           System.out.println("You have a " + c.getRank() + " of " + c.getSuitName());
+       }
     }
 }
