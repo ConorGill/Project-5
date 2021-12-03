4c80289af0c53740adceaeab1c3aa8e3e765fddb
ConorGill <94633176+ConorGill@users.noreply.github.com>
2021-12-02 23:54:56 -0500

    Update Card.java
    Had to add some new private variables because of some weird conflict because of a static context I didn't really understand it but I still changed the program.  I created the Value variable to represent the value of the card and made appropriate methods.  Did the same by adding a new private variable Suit which is a string of the suit name as well as appropriate methods to set and get that value.

--- Labz-copy/Card.java
+++ Labz-copy/Card.java
@@ -7,9 +7,13 @@ public class Card
 {
     private Rank rank;
     private Suit suit;
+    private int Value;
+    private String Suit;
     public Card(Rank r, Suit s){
-        rank=r;
-        suit=s;
+        this.rank=r;
+        this.suit=s;
+        determineValue(this.rank);
+        determineSuit(this.suit);
     }
     /*Desc: Get this Card's Rank
      * @param rank: rank of the card
@@ -24,5 +28,68 @@ public class Card
      */
     public Suit getSuit(){
         return this.suit;
+    }
+    /*
+     * Desc: Determines the value of a card
+     * @param r: Rank of the current card
+     * TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
+     */
+    public void determineValue(Rank r){
+        int x=0;
+        switch(r){
+            case ACE: x=1;
+            break;
+            case TWO: x=2;
+            break;
+            case THREE: x=3;
+            break;
+            case FOUR: x=4;
+            break;
+            case FIVE: x=5;
+            break;
+            case SIX: x=6;
+            break;
+            case SEVEN: x=7;
+            break;
+            case EIGHT: x=8;
+            break;
+            case NINE: x=9;
+            break;
+            case TEN: x=10;
+            break;
+            case JACK: x=10;
+            break;
+            case QUEEN: x=10;
+            break;
+            case KING: x=10;
+            break;
+        }
+        this.Value=x;
+    }
+    //Desc: Returns the current Card's value
+    public int getValue(){
+        return this.Value;
+    }
+    /* Desc: Sets a string variable with the same name as the suit
+     * @param s: Suit of the current Card
+     * CLUBS,SPADES,DIAMONDS,HEARTS
+     */
+    public void determineSuit(Suit s){
+        String x="";
+        switch(s){
+            case CLUBS: x="Clubs";
+            break;
+            case SPADES: x="Spades";
+            break;
+            case DIAMONDS: x="Diamonds";
+            break;
+            case HEARTS: x="Hearts";
+            break;
+        }
+        this.Suit=x;
+    }
+    //Desc: Returns the name of the Suit
+    public String getSuitName(){
+        return this.Suit;
     }
 }
