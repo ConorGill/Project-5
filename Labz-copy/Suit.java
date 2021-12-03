5c4b82735d1b2f851732a930d5d9bd644f4bdeb3
ConorGill <94633176+ConorGill@users.noreply.github.com>
2021-12-02 23:06:32 -0500

    Create Suit.java
    Renamed Type to Suit because that makes more sense

---
+++ Labz-copy/Suit.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Colors - write a description of the enum class here
+ * Made a value for every possible type of color
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Suit
+{
+    CLUBS,SPADES,DIAMONDS,HEARTS
+}
