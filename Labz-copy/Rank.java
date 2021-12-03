38f0f772063a9eb4c71ed75a5119ae8931e1d046
ConorGill <94633176+ConorGill@users.noreply.github.com>
2021-12-02 23:07:08 -0500

    Create Rank.java
    Renamed Cards to Rank because Cards was a confusing name

---
+++ Labz-copy/Rank.java
@@ -0,0 +1,11 @@
+
+/**
+ * Enumeration class Cards - write a description of the enum class here
+ * Made a value for the number of every possible card.
+ * @author (your name here)
+ * @version (version number or date here)
+ */
+public enum Rank
+{
+    TWO,THREE,FOUR,FIVE,SIX,SEVEN,EIGHT,NINE,TEN,JACK,QUEEN,KING,ACE
+}
